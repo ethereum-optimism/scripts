468d2a0c31e2b3b87e79b92dbee98c1a04982ee5
Matt Masurka <m.masurka@gmail.com>
2021-02-22 17:06:29 -0800

    Adds script for dumping verifier and sequencer state

--- scripts/verifier/diff-verifier-sequencer-state-dump.js
+++ scripts/verifier/diff-verifier-sequencer-state-dump.js
@@ -6,29 +6,72 @@
  * VERIFIER_ENDPOINT: RPC endpoint for the verifier node.
  * SEQUENCER_ENDPOINT: RPC endpoint for the sequencer node.
  */
+require("colors");
+const axios = require("axios");
+const diff = require("deep-diff").diff;
+const fs = require("fs");
+const log = require("single-line-log").stdout;
+const dotenv = require("dotenv").config();
+const { JsonRpcProvider } = require("@ethersproject/providers");
 
-const log = require('single-line-log').stdout
-const dotenv = require('dotenv')
-const { JsonRpcProvider } = require('@ethersproject/providers')
+const sources = {
+  kovan: [
+    { url: "https://raw.githubusercontent.com/ethereum-optimism/regenesis/master/kovan/1.json", path: "accounts" },
+    {
+      url: "https://raw.githubusercontent.com/Synthetixio/synthetix/develop/publish/deployed/kovan-ovm/deployment.json",
+      path: "targets",
+    },
+  ],
+  mainnet: [
+    { url: "", path: "accounts" },
+    { url: "", path: "targets" },
+  ],
+};
 
 const getBlock = async (provider, index) => {
-  return provider.send('eth_getBlockByNumber', [`0x${index.toString(16)}`, true])
-}
+  return provider.send("eth_getBlockByNumber", [`0x${index.toString(16)}`, true]);
+};
+
+const getAddressMapping = async () => {
+  const addressMapping = {};
+
+  for (const addressItem of sources[process.env.ETH_NETWORK]) {
+    let { data: contractInfo } = await axios.get(addressItem.url);
+    contractInfo = contractInfo[addressItem.path];
+    for (const contractName in contractInfo) {
+      const contractAddress = contractInfo[contractName].address;
+      addressMapping[contractAddress] = contractName;
+    }
+  }
+  return addressMapping;
+};
 
 async function main() {
-  dotenv.config()
+  const addressMapping = await getAddressMapping();
+  const verifier = new JsonRpcProvider(process.env.VERIFIER_ENDPOINT);
+  const sequencer = new JsonRpcProvider(process.env.SEQUENCER_ENDPOINT);
+  console.log("----------getting Verifier dump-----------");
 
-  const verifier = new JsonRpcProvider(process.env.VERIFIER_ENDPOINT)
-  const sequencer = new JsonRpcProvider(process.env.SEQUENCER_ENDPOINT)
-  console.log('getting Verifier dump')
   // get current block number for verifier and sequeuncer
-  // make sure block number is same, otherwise use lower block number
-  // const vdump = await verifier.send('debug_dumpBlock',[blocknumber])
-  // const sdump = await sequencer.send('debug_dumpBlock',[blocknumber])
-  // store the dump in a JSON file
-  // diff the dumps - make sure to log any storage slots that are in one and not the other or have values that are different between the two.
-  // Diff any mismatched code
-  // Log the diff in an easy to view format
+  const latestVerifierBlockNum = await verifier.getBlockNumber();
+  const latestSequencerBlockNum = await sequencer.getBlockNumber();
+
+  // use lower block number
+  const latestBlockNum = Math.min(latestVerifierBlockNum, latestSequencerBlockNum);
+
+  console.log(`Getting the verfier data...`);
+
+  let vdump = await verifier.send("debug_dumpBlock", [`0x${latestBlockNum.toString(16)}`]);
+
+  console.log(`Getting the sequencer data...`);
+  let sdump = await sequencer.send("debug_dumpBlock", [`0x${latestBlockNum.toString(16)}`]);
+
+  console.log(`Stringifying data...`);
+
+  console.log(`Making the diff...`);
+  var differences = diff(vdump, sdump);
+  console.log(differences);
+  fs.writeFileSync("diff.json", JSON.stringify(differences, null, 2));
 }
 
-main()
+main();
