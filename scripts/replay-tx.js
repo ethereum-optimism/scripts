e0ad11c176ae732c28d9681aa7cc23d62d89b2ac
Matt Masurka <m.masurka@gmail.com>
2021-05-20 19:18:17 -0700

    Adding loop-over-history functionality to replay-tx script

--- scripts/replay-tx.js
+++ scripts/replay-tx.js
@@ -1,3 +1,8 @@
+/**
+ * Message replayer script
+ * Requires a hash, network (string), and hash or block number
+ * If block number provided, it loops over the transaction history and replays all failed txs
+ */
 require("colors");
 const { Command } = require("commander");
 const log = require("single-line-log").stdout;
@@ -7,25 +12,43 @@ const { getContractInterface } = require("@eth-optimism/contracts");
 const { JsonRpcProvider, getDefaultProvider } = require("@ethersproject/providers");
 const program = new Command();
 
-program.requiredOption("-h, --hash <number>", "specify transaction hash");
 program.requiredOption("-n, --network <string>", "specify network");
-program.option("-k, --key <number>", "specify private key");
+program.requiredOption("-k, --key <number>", "specify private key");
+program.option("-h, --hash <number>", "specify transaction hash");
+program.option("-b, --blockNumber <number>", "specify a block number");
+
 program.parse(process.argv);
 const argOptions = program.opts();
 
 const mainnetProvider = getDefaultProvider(argOptions.network);
 
-const L1_MESSENGER_PROXY = "0xD1EC7d40CCd01EB7A305b94cBa8AB6D17f6a9eFE";
+// Addresses from May 11 regenesis
+const MAINNET_L1_MESSENGER_PROXY = "0x902e5fF5A99C4eC1C21bbab089fdabE32EF0A5DF";
+const KOVAN_L1_MESSENGER_PROXY = "0x78b88FD62FBdBf67b9C5C6528CF84E9d30BB28e0";
 
 const wallet = new Wallet(argOptions.key, mainnetProvider);
 const proxyL1Messenger = new Contract(L1_MESSENGER_PROXY, getContractInterface("OVM_L1CrossDomainMessenger"), wallet);
 
 const main = async () => {
-  const receipt = await mainnetProvider.getTransactionReceipt(argOptions.hash);
+  if (!argOptions.hash && !argOptions.blockNumber) {
+    console.error("Must provide a hash or block number.");
+    return;
+  }
+
+  if (argOptions.hash) {
+    replayMessage(argOptions.hash);
+  } else {
+    // TODO: loop over blocks starting at argOptions.blockNumber, get all failed transactions and replay them
+  }
+};
+
+const replayMessage = async (hash) => {
+  const l1MessengerProxy = argOptions.network === "mainnet" ? MAINNET_L1_MESSENGER_PROXY : KOVAN_L1_MESSENGER_PROXY;
+  const receipt = await mainnetProvider.getTransactionReceipt(hash);
 
   const decodedMessages = [];
   for (const log of receipt.logs) {
-    if (log.address === L1_MESSENGER_PROXY && log.topics[0] === utils.id("SentMessage(bytes)")) {
+    if (log.address === l1MessengerProxy && log.topics[0] === utils.id("SentMessage(bytes)")) {
       const [message] = utils.defaultAbiCoder.decode(["bytes"], log.data);
 
       const xDomainInterface = getContractInterface("OVM_L2CrossDomainMessenger");
