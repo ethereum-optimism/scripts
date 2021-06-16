715b81e7519312ecfb152ce4ed361e3ea26c801e
Mark Tyneway <mark.tyneway@gmail.com>
2021-06-16 10:23:29 -0700

    scripts: fix

--- scripts/make-slots.js
+++ scripts/make-slots.js
@@ -3,14 +3,11 @@ const ethers = require('ethers')
 const makeStorageFromAccounts = (accounts) => {
   const storage = {}
   for (const [address, balance] of Object.entries(accounts)) {
-    // Key for a mapping at index 5
     const preimage = ethers.utils.hexConcat([
       ethers.utils.hexZeroPad(address, 32),
-      ethers.utils.hexZeroPad('0x05', 32),
+      ethers.utils.hexZeroPad('0x00', 32),
     ])
     const key = ethers.utils.keccak256(preimage)
-
-    // Uint256 is right-padded.
     const val = ethers.BigNumber.from(balance).toHexString()
     storage[key] = val
   }
